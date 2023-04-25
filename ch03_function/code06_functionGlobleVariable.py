67a2be069e7f1f7a0d4e9177696d830e38565fa7
Jacket777 <hxqsu37@sina.com>
2023-04-25 21:36:20 +0800

    函数中局部变量与全局变量效率比较与函数名正确使用

--- ch03_function/code06_functionGlobleVariable.py
+++ ch03_function/code06_functionGlobleVariable.py
@@ -1,21 +1,25 @@
-#测试局部变量，全局变量的效率
+# 测试局部变量，全局变量的效率
 import math
 import time
+
 
 def test01():
     start = time.time()
     for i in range(10000000):
         math.sqrt(30)
     end = time.time()
-    print("耗时{0}".format((end-start)))
+    print("耗时{0}".format((end - start)))
+
 
 def test02():
-    b=math.sqrt
+    b = math.sqrt
     start = time.time()
     for i in range(10000000):
         b(30)
     end = time.time()
     print("耗时{0}".format((end - start)))
 
-test01()
-test02()
+
+if __name__ == '__main__':
+    test01()
+    test02()
