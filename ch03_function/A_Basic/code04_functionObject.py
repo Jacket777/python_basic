8d1445be94ccf3cc20cfa0c17ea2319465fee179
Jacket777 <hxqsu37@sina.com>
2023-04-25 21:20:21 +0800

    函数对象，有参与无参函数

--- ch03_function/A_Basic/code04_functionObject.py
+++ ch03_function/A_Basic/code04_functionObject.py
@@ -1,12 +1,27 @@
-# 测试函数也是对象
+# 测试函数也是对象 函数名其实是指向函数对象的一个引用
 def test01():
+    """无参函数"""
     print("sx")
 
 
-test01()
-print("=============")
-c = test01
-c()
-print(id(test01))
-print(id(c))
-print(type(c))
+def test02(a, b):
+    return a + b
+
+
+if __name__ == '__main__':
+    test01()
+    print("=============")
+    c = test01
+    c()
+    print(id(test01))
+    print(id(c))
+    print(id(test01) == id(c))
+    print(type(c))
+    print("-----------")
+    other=test02
+    print(id(test02))
+    print(id(other))
+    print(other(3,4))
+
+
+
