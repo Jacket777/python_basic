b5b3e60ba121c7259756ed51e974a417e7f9e27f
Jacket777 <hxqsu37@sina.com>
2023-05-10 09:12:26 +0800

    方法动态性

---
+++ ch09_Class/part04_poly/code03_dynamic_method.py
@@ -0,0 +1,27 @@
+"""
+方法的动态性
+方法可以随时绑定给对象
+"""
+
+
+class Person:
+    def work(self):
+        print("work hard")
+
+
+def play_game(s):
+    print("{0}在玩游戏".format(s))
+
+
+def work2(s):
+    print("work hard======")
+
+
+if __name__ == '__main__':
+    Person.play = play_game;
+    p = Person()
+    p.work()
+    p.play()
+
+    Person.work = work2
+    p.work()
